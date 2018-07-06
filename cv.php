f6bbec328d588f0ebfeb5b69b51c9fd2b1929fd7
BEN REJEB Mohamed hedi <mohamedhedi.benrejeb@esprit.tn>
2018-07-06 11:05:14 +0200

    mise à jour de cv.php

---
+++ cv.php
@@ -0,0 +1,15 @@
+<?php
+include 'personal_details.php';
+?>
+<?php
+include 'left_info.php';
+?>			
+<?php
+include 'education.php';
+?>
+<?php
+include 'academic.php';
+?>	
+<?php
+include 'professional.php';
+?>	
